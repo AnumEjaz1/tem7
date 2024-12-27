4ba68cccb8ec8949560ebd3280ce93f4e89523e7
AnumEjaz1 <anumejaz2015@gmail.com>
2024-12-27 19:31:05 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+// pages/index.tsx
+import { IMAGES_MANIFEST } from 'next/dist/shared/lib/constants';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Footer/>
+     
+    </div>
+  );
+};
+
+export default Home;
